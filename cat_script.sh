b61a03e17fa57dcdbf8d07740d2a1139f6b1a0b2
fishCookie <WUYU44577984@163.com>
2024-09-14 02:25:43 +0800

    Update cat_script.sh

--- cat_script.sh
+++ cat_script.sh
@@ -1,4 +1,4 @@
-command="yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5 --fee-rate 300"
+command="yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5 --fee-rate 1300"
 
 while true; do
     $command
